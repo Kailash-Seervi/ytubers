b51df7ed43adadc466df43e1574df74a5969dca5
Kailash R <kseervi257@gmail.com>
2020-12-07 11:09:03 +0530

    first commit

---
+++ tubers/webpages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'webpages/home.html')
+
+def about(request):
+    return render(request, 'webpages/about.html')
+
+def services(request):
+    return render(request, 'webpages/services.html')
+
+def contact(request):
+        return render(request, 'webpages/contact.html')
